e593effab527389e8901b47d10170aa09c016f7d
Sean R MacEachern <sean.r.maceachern@gmail.com>
2020-12-14 21:27:51 -0500

    all changes leading up to polymorphism, as well took all work from pets2 and transferred it to pets

--- pets.py
+++ pets.py
@@ -1,58 +1,39 @@
-# Define a dictionary that holds our pet's attributes.
-puppy_1 = {
-    "name": "Cujo",
-    "fullness": 50,
-    "happiness": 20,
-    "hunger": 7,
-    "mopiness": 4,
-}
-puppy_2 = {
-    "name": "Benji",
-    "fullness": 50,
-    "happiness": 100,
-    "hunger": 3,
-    "mopiness": 2,
-}
-pets = [puppy_1, puppy_2]
+class Pet:
+    def __init__(self, name, fullness=50, happiness=50, hunger=5, mopiness=5):
+        self.name = name
+        self.fullness = fullness
+        self.happiness = happiness
+        self.hunger = hunger
+        self.mopiness = mopiness
 
-# Define functions that increase a pet's attribute levels.
-def feed_pet(pet):
-    pet["fullness"] += 10
+    def eat_food(self):
+        self.fullness += 30
 
+    def get_love(self):
+        self.happiness += 30
 
-def play_with_pet(pet):
-    pet["happiness"] += 10
+    def be_alive(self):
+        self.fullness -= self.hunger
+        self.happiness -= self.mopiness
 
-# Decrease a pet's attribute levels.
-def get_hungry_and_mopey(pet):
-    pet["fullness"] -= pet["hunger"]
-    pet["happiness"] -= pet["mopiness"]
+        
+class CuddlyPet(Pet):
+    def __init__(self, name, fullness=50, hunger=5, cuddle_level=1):
+        super().__init__(name, fullness, 100, hunger, 1)
+        self.cuddle_level = cuddle_level
+    
+    def be_alive(self):
+        self.fullness -= self.hunger
+        self.happiness -= self.mopiness/2
+        
+    def cuddle(self, other_pet):
+        # Super cuddle powers, activate!
+        for i in range(self.cuddle_level):
+            other_pet.get_love()
 
+benji = CuddlyPet("Benji")
+cujo = Pet("Cujo")
+print(cujo.name, cujo.fullness, cujo.happiness, cujo.hunger, cujo.mopiness)
 
-# Prompt the user to interact with the pet
-while True:
-    for pet in pets:
-    print("""
-%s's stats:
-Fullness: %d
-Happiness: %d
-""" % (pet["name"], pet["fullness"], pet["happiness"]))
-
-    choice = int(input("""
-1. Feed puppy
-2. Play with puppy
-3. Do nothing
-"""))
-    if choice == 1:
-        which_pet = int(input("Which pet?"))
-        feed_pet(pets[which_pet])
-    elif choice == 2:
-        which_pet = int(input("Which pet?"))
-        play_with_pet(pets[which_pet])
-    else:
-        pass
-
-    # Each time the loop runs, the pet
-    # will need some attention!
-    for pet in pets:
-        get_hungry_and_mopey(pet)
+benji.cuddle(cujo)
+print(cujo.name, cujo.fullness, cujo.happiness, cujo.hunger, cujo.mopiness)
--- pets2.py
+++ pets2.py
@@ -18,26 +18,23 @@ class Pet:
 
 
 class CuddlyPet(Pet):
-    def __init__(self, name, fullness=50, happiness=50, hunger=5, mopiness=5):
-        self.name = name
-        self.fullness = fullness
-        self.happiness = 100
-        self.hunger = hunger
-        self.mopiness = 1
-        
+    def __init__(self, name, fullness=50, hunger=5, cuddle_level=1):
+        super().__init__(name, fullness, 100, hunger, 1)
+        self.cuddle_level = cuddle_level
+
     def be_alive(self):
         self.fullness -= self.hunger
         self.happiness -= self.mopiness/2
 
     def cuddle(self, other_pet):
-        other_pet.get_love()
+        # SUPER CUDDLE POWERS, ACTIVATED!
+        for i in range(self.cuddle_level):
+            other_pet.get_love()
 
+benji = CuddlyPet("Benji")
+cujo = Pet("Cujo")
+print(cujo.name, cujo.fullness, cujo.happiness, cujo.hunger, cujo.mopiness)
 
-
-benji = CuddlyPet("Benji", 50, 20, 20, 1)
-cujo = Pet("Cujo", 50, 10, 30, 10) # CUJO IS DIFFERENT FROM BENJI BECAUSE HE IS NOT AS HAPPY AS BENJI TO START OFF.
-print(cujo.happiness)
-# 10
 benji.cuddle(cujo)
-print(cujo.happiness)
-# 40    BUT FOR SOME REASON IM ONLY GETTING 10
+print(cujo.name, cujo.fullness, cujo.happiness, cujo.hunger, cujo.mopiness)
+
