d64030b1f8cbd9c4a99b7cb4d9c7d679fa8206df
Sean R MacEachern <sean.r.maceachern@gmail.com>
2020-12-14 21:03:18 -0500

    step_3_inheritance before taking out happiness and mopiness arguments

--- pets2.py
+++ pets2.py
@@ -18,6 +18,17 @@ class Pet:
 
 
 class CuddlyPet(Pet):
+    def __init__(self, name, fullness=50, happiness=50, hunger=5, mopiness=5):
+        self.name = name
+        self.fullness = fullness
+        self.happiness = 100
+        self.hunger = hunger
+        self.mopiness = 1
+        
+    def be_alive(self):
+        self.fullness -= self.hunger
+        self.happiness -= self.mopiness/2
+
     def cuddle(self, other_pet):
         other_pet.get_love()
 
